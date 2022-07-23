f92d264a983459b80ff6bcb39ef9165ca9f4d7ee
prettyink <83136004+prettyink@users.noreply.github.com>
2022-07-22 22:20:00 -0400

    adding tarot

---
+++ tarot/tarot_cards.py
@@ -0,0 +1,143 @@
+card_list: dict = {
+    "1": {
+        "card_meaning": "Harmonious home. Nostalgia. Children and childhood memories.",
+        "card_name": "April",
+        "card_img": "https://i.imgur.com/aiXMIwX.png",
+    },
+    "2": {
+        "card_meaning": "Wisdom gained after struggle. The ending of a difficult situation.",
+        "card_name": "Alan",
+        "card_img": "https://i.imgur.com/IT0qd4N.png",
+    },
+    "3": {
+        "card_meaning": "Important decisions or news. Movement into the next phase of life.",
+        "card_name": "Bernie",
+        "card_img": "https://i.imgur.com/5AvF20E.png",
+    },
+    "4": {
+        "card_meaning": "The need to allow something to die to create room for the new.",
+        "card_name": "Blair",
+        "card_img": "https://i.imgur.com/xOC1Ufq.png",
+    },
+    "5": {
+        "card_meaning": "Great satisfaction. Contentment. Earthly delights.",
+        "card_name": "Bryce",
+        "card_img": "https://i.imgur.com/l7peFyB.png",
+    },
+    "6": {
+        "card_meaning": "The desire for marriage or some other formal structure for security",
+        "card_name": "Chad",
+        "card_img": "https://i.imgur.com/M0vGhUz.png",
+    },
+    "7": {
+        "card_meaning": "Overindulging in thoughts of what the future may bring. Daydreams.",
+        "card_name": "Courtney",
+        "card_img": "https://i.imgur.com/krzqMqE.png",
+    },
+    "8": {
+        "card_meaning": "The ability to create wealth. Investments — money or emotional.",
+        "card_name": "Dean Monroe",
+        "card_img": "https://i.imgur.com/Dswt1h7.png",
+    },
+    "9": {
+        "card_meaning": "The ability to use power wisely. Leadership and Inspiration.",
+        "card_name": "Demi",
+        "card_img": "https://i.imgur.com/JGYpsaM.png",
+    },
+    "10": {
+        "card_meaning": "True generosity and philanthropy. Sharing talents.",
+        "card_name": "Dr. Gray",
+        "card_img": "https://i.imgur.com/hOji9jJ.png",
+    },
+    "11": {
+        "card_meaning": "A new phase of life with prosperity, fertility & generosity",
+        "card_name": "Gabe",
+        "card_img": "https://i.imgur.com/wLJeBZD.png",
+    },
+    "12": {
+        "card_meaning": "Feeling rejection. Entrapped by emotions and sorrow.",
+        "card_name": "Grady",
+        "card_img": "https://i.imgur.com/PtQxikl.png",
+    },
+    "13": {
+        "card_meaning": "Renewed creativity and vigor. New awareness of one’s power.",
+        "card_name": "Jace",
+        "card_img": "https://i.imgur.com/74z2FNw.png",
+    },
+    "14": {
+        "card_meaning": "Exhaustion or overresponsibility. A pause to reconsider.",
+        "card_name": "Jay",
+        "card_img": "https://i.imgur.com/M97lB5w.png",
+    },
+    "15": {
+        "card_meaning": "Time for a new start of a great journey.",
+        "card_name": "Luke",
+        "card_img": "https://i.imgur.com/Rh6JmrG.png",
+    },
+    "16": {
+        "card_meaning": "The ability to express love, to nurture others.",
+        "card_name": "Jose Arroyo",
+        "card_img": "https://i.imgur.com/UurbINE.png",
+    },
+    "17": {
+        "card_meaning": "Back-stabbed, defeat, crisis, betrayal, endings, loss",
+        "card_name": "Joe Helms",
+        "card_img": "https://i.imgur.com/Z5pOZng.png",
+    },
+    "18": {
+        "card_meaning": "Being caught between worlds. Short term sacrifice.",
+        "card_name": "Pierce",
+        "card_img": "https://i.imgur.com/LNLB5xz.png",
+    },
+    "19": {
+        "card_meaning": "News that brings sense to a situation. Understanding.",
+        "card_name": "Professor McGregor",
+        "card_img": "https://i.imgur.com/yILzEZe.png",
+    },
+    "20": {
+        "card_meaning": "An experience of poverty.",
+        "card_name": "Rafi",
+        "card_img": "https://i.imgur.com/mlNOokn.png",
+    },
+    "21": {
+        "card_meaning": "Pleasure or sensuality. Feeling blessed by life.",
+        "card_name": "Ruby",
+        "card_img": "https://i.imgur.com/3evsvuM.png",
+    },
+    "22": {
+        "card_meaning": "Expectations of reward. Waiting for a harvest.",
+        "card_name": "Samir",
+        "card_img": "https://i.imgur.com/PYpvTl5.png",
+    },
+    "23": {
+        "card_meaning": "Feeling overwhelmed or oppressed by circumstances or emotions.",
+        "card_name": "Shaun",
+        "card_img": "https://i.imgur.com/NJ0T9Pt.png",
+    },
+    "24": {
+        "card_meaning": "Cutting through confusion. Focus, and clarity. ",
+        "card_name": "Thomas",
+        "card_img": "https://i.imgur.com/DhECuPd.png",
+    },
+    "25": {
+        "card_meaning": "Cumulation of a creative venture.",
+        "card_name": "Triggs",
+        "card_img": "https://i.imgur.com/Le0ZZWI.png",
+    },
+    "26": {
+        "card_meaning": "Exhaustion or overresponsibility.",
+        "card_name": "Trish",
+        "card_img": "https://i.imgur.com/Qy9GrTz.png",
+    },
+    "27": {
+        "card_meaning": "Feelings of expansion. Capriciousness.",
+        "card_name": "Stalker-Chan",
+        "card_img": "https://i.imgur.com/J9URpWG.png",
+    },
+    "28": {
+        "card_meaning": "Understanding a difficult situation. Emotional control.",
+        "card_name": "Nobody",
+        "card_img": "https://i.imgur.com/etAmbaz.png",
+    },
+    
+}
